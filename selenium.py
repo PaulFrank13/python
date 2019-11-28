65dd046b39a71bfe07fd71c86e5a3a9f600c245b
PaulFrank13 <58308617+PaulFrank13@users.noreply.github.com>
2019-11-28 23:43:05 +0500

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 
+stealth_mode = False
+
 options = webdriver.ChromeOptions()
 options.binary_location = ""
 options.add_argument('log-level=3')
 options.headless = True
 
 if stealth_mode:
-    printTime('Запуск браузера в скрытом режими.')
+    print('Запуск браузера в скрытом режими.')
 else:
-    printTime('Запуск браузера в обычном режими.')
+    print('Запуск браузера в обычном режими.')
 
 driver = webdriver.Chrome(options=options)
